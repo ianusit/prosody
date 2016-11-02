cc3a7bc4f6739dd6d9c6a411cdd82642c49b151d
amuehlbauer <andreas.muehlbauer@ianus-it.de>
2016-11-02 14:34:44 +0100

    Create start.sh

---
+++ files/start.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+sed -i "s/VHOST/${VHOST}/g" /etc/prosody/prosody.cfg.lua
+sed -i "s/SSLCERT/${SSLCERT}/g" /etc/prosody/prosody.cfg.lua
+sed -i "s/SSLKEY/${SSLKEY}/g" /etc/prosody/prosody.cfg.lua
+prosody
