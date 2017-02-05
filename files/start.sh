eab0a91590676f6b25b7592b2eed33b9001b4405
amuehlbauer <andreas.muehlbauer@ianus-it.de>
2017-02-05 08:59:39 +0100

    Update start.sh

--- files/start.sh
+++ files/start.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 sed -i "s/VHOST/${VHOST}/g" /etc/prosody/prosody.cfg.lua
-sed -i "s/SSLCERT/${SSLCERT}/g" /etc/prosody/prosody.cfg.lua
-sed -i "s/SSLKEY/${SSLKEY}/g" /etc/prosody/prosody.cfg.lua
 prosodyctl start
